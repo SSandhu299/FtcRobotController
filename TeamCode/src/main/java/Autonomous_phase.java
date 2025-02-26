33d19959772d4c3203c87b2d3318bcd865075ee3
SSandhu299 <sukhmans026@deltalearns.ca>
2025-02-25 21:06:50 -0800

    Intial commit
    This is an initial commit that introduced the two files of code I will be working on: Autonomous and teleop

---
+++ TeamCode/src/main/java/Autonomous_phase.java
@@ -0,0 +1,3 @@
+// This is the code for the autonomous part of the ftc match. The autonomous period goes on for 30 seconds
+
+// import needed packages
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_phase.java
@@ -0,0 +1,3 @@
+// This is the code for the teleop phase of the code - where players put live inputs to control how the robots movements
+
+// import needed packages
